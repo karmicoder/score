a3c8ac84a883550ee0d6168e7f80d157e82ab673
Michael <mosullivan.8@gmail.com>
2022-12-23 21:22:00 -0700

    basic timer

---
+++ src/components/game-clock.svelte
@@ -0,0 +1,92 @@
+<script lang="ts" context="module">
+	function formatTimePart(timePart: number) {
+		return timePart.toString().padStart(2, '0');
+	}
+</script>
+
+<script lang="ts">
+	import moment from 'moment';
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher<{ horn: () => void }>();
+	const interval = 1000 / 60; // 1/60 sec
+	export let duration: moment.Duration;
+
+	let remainingDuration = duration;
+	let intervalRef: NodeJS.Timer | undefined;
+	let lastIntervalTime: moment.Moment | undefined;
+	$: {
+		if (remainingDuration.asMilliseconds() <= 0) {
+			remainingDuration = moment.duration(0);
+			handlePause();
+			dispatch('horn');
+		}
+	}
+	$: isRunning = !!intervalRef;
+	$: isHorn = remainingDuration.asMilliseconds() <= 0;
+
+	let handleStart = () => {
+		lastIntervalTime = moment();
+		intervalRef = setInterval(() => {
+			remainingDuration = remainingDuration
+				.clone()
+				.subtract(moment().diff(lastIntervalTime, 'ms'), 'ms');
+			lastIntervalTime = moment();
+		}, interval);
+	};
+
+	let handlePause = () => {
+		clearInterval(intervalRef);
+		intervalRef = undefined;
+	};
+
+	let handleReset = () => {
+		remainingDuration = duration;
+	};
+</script>
+
+<div class="GameClock">
+	<div class="display">
+		{#if remainingDuration.asHours() >= 1}{formatTimePart(
+				remainingDuration.hours()
+			)}:{/if}{#if remainingDuration.asMinutes() >= 1}{formatTimePart(
+				remainingDuration.minutes()
+			)}:{/if}{formatTimePart(
+			remainingDuration.seconds()
+		)}{#if remainingDuration.asMinutes() < 1}.{remainingDuration
+				.milliseconds()
+				.toString()
+				.slice(0, 2)
+				.padStart(2, '0')}{/if}
+	</div>
+	<div class="actions">
+		<button type="button" on:click={handleStart} disabled={isRunning || isHorn}>Start</button>
+		<button type="button" on:click={handlePause} disabled={!isRunning}>Stop</button>
+		<button type="button" on:click={handleReset}>Reset</button>
+	</div>
+	{#if isHorn}Horn!{/if}
+</div>
+
+<style lang="scss">
+	.GameClock {
+		padding: 1rem;
+		width: 25%;
+		min-width: 12rem;
+
+		.display {
+			font-size: 2rem;
+			font-family: monospace;
+			text-align: center;
+			border: 1px solid orange;
+		}
+
+		.actions {
+			margin-top: 1rem;
+			display: flex;
+			column-gap: 1rem;
+			justify-content: center;
+			button {
+				flex-grow: 1;
+			}
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import moment from 'moment';
+	import GameClock from '../components/game-clock.svelte';
+	let duration = moment.duration(20, 'minutes');
+	let durationString = duration.toISOString();
+</script>
+
+<h1>Score</h1>
+<div class="field">
+	<label for="duration">Duration:</label>
+	<input
+		type="text"
+		name="duration"
+		bind:value={durationString}
+		on:change={(e) => (duration = moment.duration(durationString))}
+	/>
+</div>
+<GameClock {duration} />
